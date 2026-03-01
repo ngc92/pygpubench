2fbd5bc5fd251cd0e5302409a9487657fb799d33
Erik Schultheis <erik.schultheis@ist.ac.at>
2026-03-01 23:23:50 +0100

    expand args for kernel call

--- python/pygpubench/_types.py
+++ python/pygpubench/_types.py
@@ -1,11 +1,10 @@
 from typing import Callable, Tuple
 
 Tensor = "torch.Tensor"
-KernelArgs = Tuple
 ExpectedResult = Tuple[Tensor] | Tuple[Tensor, float, float]
 
-KernelFunction = Callable[[KernelArgs], None]
+KernelFunction = Callable[..., None]
 KernelGeneratorInterface = Callable[[], KernelFunction]
-TestGeneratorInterface = Callable[..., Tuple[KernelArgs, ExpectedResult]]
+TestGeneratorInterface = Callable[..., Tuple[Tuple, ExpectedResult]]
 
 __all__ = ["KernelFunction", "KernelGeneratorInterface", "TestGeneratorInterface", "ExpectedResult"]
